d7d39dc1ddbc32562c6c1bade91d6dbb6907362b
Alberto Molina <albertomolinadelafuente@gmail.com>
2025-03-18 12:53:03 +0100

    añadiendo comentarios

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 // Server creation and configuration
 const http = require("node:http");
-const app = require("./src/app");
+const app = require("./src/app"); // Importamos todo lo que tiene el archivo app.js
 
-// Config .env
+// Config .env, lectura del fichero de entorno donde van todo los datos de MySQL(nombre de la base de datos, usuario, contraseña, etc)
 require("dotenv").config();
 
 // Server creation
 const server = http.createServer(app);
 
 const PORT = process.env.PORT || 3000;
+
+// Puesta en marcha del servidor
 server.listen(PORT);
 
 // Listeners
 server.on("listening", () => {
-    console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
 
 server.on("error", (error) => {
-    console.log(error);
+  console.log(error);
 });
--- src/app.js
+++ src/app.js
@@ -2,25 +2,25 @@
 const express = require("express");
 const cors = require("cors");
 
-const app = express();
-app.use(express.json());
-app.use(cors());
+const app = express(); // Creación de la aplicación de express
+app.use(express.json()); // Middleware para que la aplicación entienda JSON y pueda recibir objetos en formato JSON
+app.use(cors()); // Middleware para que la aplicación entienda CORS y permitir peticiones desde cualquier origen
 
 // Route configuration
 // Ex.
 // app.use('/api', require('./routes/api.routes'));
 
-// 404 handler
+// Esto es un manejador para los errores 404, por si la ruta no es capaz de encontrarla
 app.use((req, res, next) => {
-    res.status(404).json({
-        message: "Not found"
-    });
+  res.status(404).json({
+    message: "Not found",
+  });
 });
 
-// Error handler
+// manejador de errores de nuestras aplicaciones, todo lo que falle en nuestra aplicacion lo mando aqui para unificarlo todo y no hacerlo de manera individual
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ message: err.message });
+  console.error(err.stack);
+  res.status(500).json({ message: err.message });
 });
 
 module.exports = app;
