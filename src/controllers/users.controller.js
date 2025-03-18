e73b661f3f8b0ecbb3b0ab0e5ce012626c0a75db
Alberto Molina <albertomolinadelafuente@gmail.com>
2025-03-18 16:32:51 +0100

    muchas cosas

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,5 +1,15 @@
-const register = (req, res, next) => {
+const User = require("../models/users.model");
+
+const register = async (req, res, next) => {
   // los datos del nuevo user estan en req.body
+  try {
+    const result = await User.insert(req.body);
+    const user = await User.selectById(result.insertId);
+    // se devuelve el user recien creado
+    res.json(user);
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
